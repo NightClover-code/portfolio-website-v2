60c42f40323bc4870f79f3bb5eeadbff7c558c7d
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-11 00:01:55 +0000

    navbar initial design complete

--- components/Header/NavBar/NavItem/index.tsx
+++ components/Header/NavBar/NavItem/index.tsx
@@ -1,5 +1,5 @@
 const NavItem: React.FC = ({ children }) => {
-  return <li className="mx-6 cursor-pointer">{children}</li>;
+  return <li className="mx-5 cursor-pointer font-semibold">{children}</li>;
 };
 
 export default NavItem;
--- components/Header/NavBar/index.tsx
+++ components/Header/NavBar/index.tsx
@@ -5,11 +5,14 @@ const NavBar = () => {
   const navItems = ['Home', 'Portfolio', 'Skills', 'Services', 'About', 'Blog'];
 
   return (
-    <nav>
-      <ul className="flex">
+    <nav className="mt-1">
+      <ul className="flex items-center">
         {navItems.map(_item => {
           return <NavItem key={v4()}>{_item}</NavItem>;
         })}
+        <li>
+          <button className="contact__btn">Contact me</button>
+        </li>
       </ul>
     </nav>
   );
