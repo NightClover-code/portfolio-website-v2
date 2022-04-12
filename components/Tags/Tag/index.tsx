74355366ab7e8911dc26a0a8bf53fa1417a2f282
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-12 05:07:57 +0000

    feat: tags component state active functionality

--- components/Tags/Tag/index.tsx
+++ components/Tags/Tag/index.tsx
@@ -1,10 +1,15 @@
 interface TagProps {
   tag: string;
+  active: boolean;
 }
 
-const Tag: React.FC<TagProps> = ({ tag }) => {
+const Tag: React.FC<TagProps> = ({ tag, active }) => {
   return (
-    <button className="border-2 border-black rounded-full px-4 py-[2px] text-[14px] font-semibold">
+    <button
+      className={`border-2 border-black rounded-full px-4 py-[2px] text-[14px] font-semibold
+    ${active ? 'text-offWhite bg-blue border-blue font-[400]' : ''}
+    `}
+    >
       {tag}
     </button>
   );
--- components/Tags/index.tsx
+++ components/Tags/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { v4 } from 'uuid';
 import Tag from './Tag';
 
@@ -6,10 +7,16 @@ interface TagsProps {
 }
 
 const Tags: React.FC<TagsProps> = ({ tags }) => {
+  const [activeTag, setTag] = useState<string>(tags[0]);
+
   return (
     <div className={`flex mt-8 gap-[14px]`}>
       {tags.map(_tag => {
-        return <Tag tag={_tag} key={v4()} />;
+        return (
+          <div key={v4()} onClick={() => setTag(_tag)}>
+            <Tag tag={_tag} active={activeTag === _tag ? true : false} />
+          </div>
+        );
       })}
     </div>
   );
