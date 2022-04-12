5a8f625a4831f56f998d555808110f83918db7a0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-12 04:41:53 +0000

    refactor: more reusability in content component

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,18 +1,21 @@
 interface ContentProps {
   category: string;
   description: string;
+  withLine?: boolean;
 }
 
 const Content: React.FC<ContentProps> = ({
   children,
   description,
   category,
+  withLine,
 }) => {
   return (
     <>
       <h4 className="text-[14px] uppercase tracking-[3px]">{category}</h4>
       <h1 className="mt-1">{children}</h1>
       <p className="max-w-[400px] m-auto mt-2 text-offBlack">{description}</p>
+      {withLine && <div className="w-[56px] h-[2px] block bg-black mt-6"></div>}
     </>
   );
 };
