6fdcfadd9df65c7af04b53a4f0b96502d61883a0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-13 20:26:36 +0000

    fix: setting bg as off-white

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -23,7 +23,7 @@ const Projects = () => {
         <Tags tags={tags} />
       </div>
 
-      <div className="grid grid-cols-3 gap-10 mt-16">
+      <div className="grid grid-cols-3 gap-7 mt-16">
         <div className="block border-2 border-black h-44"></div>
         <div className="block border-2 border-black h-44"></div>
         <div className="block border-2 border-black h-44"></div>
