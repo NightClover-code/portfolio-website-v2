f93bdd4c28f51943725a041eaf7d557fd9bdbdd3
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-13 23:16:06 +0000

    fix: more thinking on how to implement portfolio item pt2

--- components/Projects/Item/index.tsx
+++ components/Projects/Item/index.tsx
@@ -3,14 +3,24 @@ import Image from 'next/image';
 interface ItemInterface {
   height: number;
   width: number;
+  url: string;
   span?: boolean;
 }
 
-const PortolioItem: React.FC<ItemInterface> = ({ width, height, span }) => {
+const PortolioItem: React.FC<ItemInterface> = ({
+  width,
+  height,
+  span,
+  url,
+}) => {
   return (
-    <div className={`border-2 border-gray ${span ? 'row-span-2' : ''}`}>
+    <div
+      className={`relative w-[${width}px] h-[${height}px] border-2 border-faded ${
+        span ? 'row-span-2' : ''
+      }`}
+    >
       <Image
-        src="/images/portfolio/frontend/team.jpg"
+        src={`/images/portfolio/frontend/${url}`}
         width={width}
         height={height}
         objectFit="cover"
--- utils/data.ts
+++ utils/data.ts
@@ -2,22 +2,27 @@ export const portfolioBoxes = [
   {
     height: 369,
     width: 337,
+    url: 'bookmark.jpg',
   },
   {
     height: 771,
     width: 390,
     span: true,
+    url: 'team.jpg',
   },
   {
     height: 337,
     width: 337,
+    url: 'easybank.jpg',
   },
   {
     height: 372,
     width: 337,
+    url: 'manage.jpg',
   },
   {
     height: 404,
     width: 337,
+    url: 'url-shortener.jpg',
   },
 ];
