cb5874372a8927bfc532032cebd86501c9120aba
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-15 23:55:34 +0000

    feat: adding temporary data before adding CMS

--- utils/data.ts
+++ utils/data.ts
@@ -1,24 +1,30 @@
 //temp data before cms
 export const portfolioItems = [
   {
-    imgSrc: '',
-    title: '',
-    description: '',
-    width: '',
-    height: '',
+    imgSrc: '/frontend/team.svg',
+    title: 'Team App website design',
+    description: `
+      Full website design and build for a concept team collaboration platform. This website also includes a beautiful blog. I’ve built the site from sratch using NextJS and GraphCMS.
+    `,
+    width: 570,
+    height: 570,
   },
   {
-    imgSrc: '',
-    title: '',
-    description: '',
-    width: '',
-    height: '',
+    imgSrc: '/frontend/url-shortener.svg',
+    title: 'Url Shortener landing page',
+    description: `
+      Full website design of an url shortener service. I’ve built the site with ReactJS, and added auth functionality using ReduxJS and Firebase.
+    `,
+    width: 570,
+    height: 466,
   },
   {
-    imgSrc: '',
-    title: '',
-    description: '',
-    width: '',
-    height: '',
+    imgSrc: '/frontend/easybank.svg',
+    title: 'Easybank website design',
+    description: `
+      Full website design for a digital banking platform landing page. I’ve built the site from sratch using GatsbyJS and ContentfulCMS.
+    `,
+    width: 570,
+    height: 570,
   },
 ];
