e391dab21d170611992d3c7d992c36c663f9a923
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-15 23:33:46 +0000

    feat: adding first item in portfolio section

--- components/Projects/Item/index.tsx
+++ components/Projects/Item/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import PrimaryButton from '../../Buttons/Primary';
 
 interface ItemInterface {}
@@ -6,13 +7,13 @@ const PortolioItem: React.FC<ItemInterface> = () => {
   const config = {};
 
   return (
-    <div className="flex">
+    <div className="flex justify-between items-center mt-28">
       <div>
         <h4 className="text-[14px] uppercase tracking-[3px]">Latest work</h4>
         <h1 className="mt-2 text-[40px] max-w-[300px]">
           Team App website design
         </h1>
-        <p className={`min-w-[400px] m-auto mt-4 text-offBlack`}>
+        <p className={`max-w-[400px] m-auto mt-4 text-offBlack`}>
           Full website design and build for a concept team collaboration
           platform. This website also includes a beautiful blog. I’ve built the
           site from sratch using NextJS and GraphCMS.
@@ -21,7 +22,26 @@ const PortolioItem: React.FC<ItemInterface> = () => {
         <PrimaryButton className="mt-8">See this project</PrimaryButton>
       </div>
 
-      <div>{/* <Image /> */}</div>
+      <div className="relative">
+        <div>
+          <Image
+            src="/images/portfolio/frontend/team.svg"
+            width={600}
+            height={600}
+            objectFit="cover"
+            alt="team" //TODO
+          />
+        </div>
+
+        <div className="absolute bottom-3 left-2 z-[-1]">
+          <Image
+            src="/images/dots.svg"
+            width={130}
+            height={257}
+            alt="dots" //TODO
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -25,7 +25,7 @@ const Projects = () => {
         <Tags tags={tags} />
       </div>
 
-      <div className="grid grid-cols-3 mt-16 gap-7 justify-center">
+      <div className="flex flex-col">
         <PortolioItem />
       </div>
     </section>
