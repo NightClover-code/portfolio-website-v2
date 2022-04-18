099c34628d9cc490516a0226c6f581b450788896
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-18 03:23:00 +0000

    feat: services grid illustration styling

--- components/Services/ServicesGrid/index.tsx
+++ components/Services/ServicesGrid/index.tsx
@@ -3,19 +3,21 @@ import Service from './Service';
 
 const ServicesGrid = () => {
   return (
-    <div className="flex">
+    <div className="flex justify-between">
       <div className="grid">
         <Service />
         <Service />
         <Service />
       </div>
 
-      <Image
-        src="/images/portfolio/frontend/team.jpg"
-        width={551}
-        height={497}
-        alt="any" //TODO
-      />
+      <div className="relative w-[551px] h-[497px]">
+        <Image
+          src="/images/portfolio/frontend/team.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="any" //TODO
+        />
+      </div>
     </div>
   );
 };
