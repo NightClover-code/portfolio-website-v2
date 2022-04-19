04afba70f4fc667f86b2fbca9f132b58b244442e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-19 03:34:47 +0000

    feat: nav list items hover states

--- components/Header/NavBar/NavItem/index.tsx
+++ components/Header/NavBar/NavItem/index.tsx
@@ -1,5 +1,9 @@
 const NavItem: React.FC = ({ children }) => {
-  return <li className="mx-5 cursor-pointer font-semibold">{children}</li>;
+  return (
+    <li className="leading-[22px] smooth relative mx-5 cursor-pointer font-semibold hover:text-blue after:absolute after:left-[50%] after:bottom-[-3px] after:translate-x-[-50%] after:scale-x-0 after:h-[1.5px] after:bg-blue after:transition-all after:duration-300 after:w-[96%] hover:after:translate-x-[-50%] hover:after:scale-100">
+      {children}
+    </li>
+  );
 };
 
 export default NavItem;
