7d942080a8e03c1601f22468236ddd260787a938
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-04-24 16:43:16 +0000

    fix: more small fixes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
           <span>Designer</span>
         </h1>
 
-        <p className="mt-5 text-[18px] max-w-[515px] xxl:text-[17px] xxl:max-w-[440px] ">
+        <p className="mt-5 text-[18px] max-w-[515px] xxl:text-[17px] xxl:max-w-[440px] hero:text-[16px]">
           I design and build beautiful websites for businesses around the globe.
           If you need a modern and powerful website, send me an email. If we are
           a good fit, I will give you a time and cost estimate.
@@ -26,7 +26,7 @@ const Hero = () => {
         <PrimaryButton className="mt-10">See my work</PrimaryButton>
       </div>
 
-      <div className="relative">
+      <div className="relative hero:mt-8">
         <div className="border-2 border-heroBorder p-6 rounded-full border-dotted">
           <div className="relative rounded-full w-[375px] h-[375px] xxl:w-[340px] xxl:h-[340px] overflow-hidden hero:w-[300px] hero:h-[300px]">
             <Image
