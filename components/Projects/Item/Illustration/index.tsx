590fcb8967be683afdae9587dfb41f9e893ff35c
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-05-05 12:33:46 +0000

    feat: responsive project items illustration

--- components/Projects/Item/Illustration/index.tsx
+++ components/Projects/Item/Illustration/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useWindowWidth } from '../../../../hooks';
 
 export interface IllustrationProps {
   width: number;
@@ -6,32 +7,34 @@ export interface IllustrationProps {
   imgSrc: string;
 }
 
-const Illustration: React.FC<any> = ({ width, height, imgSrc }) => (
-  <div className={`relative portfolio:mt-14`}>
-    <div
-      style={{
-        maxWidth: `${width}px`,
-        maxHeight: `${height}px`,
-      }}
-    >
-      <Image
-        src={`/images/portfolio/${imgSrc}`}
-        width={width}
-        height={height}
-        objectFit="cover"
-        alt="team" //TODO
-      />
-    </div>
+const Illustration: React.FC<any> = ({ width, height, imgSrc }) => {
+  const _width = useWindowWidth();
 
-    <div className="absolute -bottom-8 -left-6 z-[-1]">
-      <Image
-        src="/images/dots.svg"
-        width={130}
-        height={257}
-        alt="dots" //TODO
-      />
+  const containerWidth = _width! < 1000 ? 820 : width;
+  const containerHeight = _width! < 1000 ? height * 1.4 : height;
+
+  return (
+    <div className={`relative portfolio:mt-14`}>
+      <div className="relative mt-12">
+        <Image
+          src={`/images/portfolio/${imgSrc}`}
+          width={containerWidth}
+          height={containerHeight}
+          objectFit="cover"
+          alt="team" //TODO
+        />
+      </div>
+
+      <div className="absolute -bottom-8 -left-6 z-[-1]">
+        <Image
+          src="/images/dots.svg"
+          width={130}
+          height={257}
+          alt="dots" //TODO
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Illustration;
