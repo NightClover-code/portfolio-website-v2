897e1f2716ba52e9db21ec913492008491a3e554
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-14 20:42:41 +0100

    refactor: about component cleanup

---
+++ components/About/Content/index.tsx
@@ -0,0 +1,30 @@
+const Content = () => {
+  return (
+    <div>
+      <h4 className="text-[16px]">Personal Info</h4>
+      <h1 className="text-[40px] mt-1">About Achraf</h1>
+      <p>
+        My name is Achraf and I&apos;m a <span>Full-stack developer</span> who
+        loves to work with modern web technologies.
+      </p>
+      <p>
+        My goal is to provide value to the large community of designers and
+        developers.
+      </p>
+      <p>
+        Ever since I started my web development journey, I like to stay busy and
+        spend most of my days taking my skills to the next-level.
+      </p>
+      <p>
+        If you need a <span>custom-made</span> website, an engaging landing page
+        for your business, or a very complex web application idea:
+      </p>
+      <p>
+        Please feel free to <span className="underline">contact me</span> to
+        further discuss your project!
+      </p>
+    </div>
+  );
+};
+
+export default Content;
--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,41 +1,16 @@
 import Image from 'next/image';
+import { Waves } from '../Icons';
+import Content from './Content';
 
 const About = () => {
   return (
     <section className="mid:mt-24 about__section flex justify-between items-center mt-32 mid2:flex-col mid2:justify-center">
-      <div>
-        <h4 className="text-[16px]">Personal Info</h4>
-        <h1 className="text-[40px] mt-1">About Achraf</h1>
-        <p>
-          My name is Achraf and I&apos;m a <span>Full-stack developer</span> who
-          loves to work with modern web technologies.
-        </p>
-        <p>
-          My goal is to provide value to the large community of designers and
-          developers.
-        </p>
-        <p>
-          Ever since I started my web development journey, I like to stay busy
-          and spend most of my days taking my skills to the next-level.
-        </p>
-        <p>
-          If you need a <span>custom-made</span> website, an engaging landing
-          page for your business, or a very complex web application idea:
-        </p>
-        <p>
-          Please feel free to <span className="underline">contact me</span> to
-          further discuss your project!
-        </p>
-      </div>
+      <Content />
 
       <div className="relative">
-        <div className="absolute -left-24 top-32 z-[100] tiny:hidden">
-          <Image src="/images/waves.svg" width={181} height={116} alt="waves" />
-        </div>
+        <Waves left={-96} top={128} />
 
-        <div className="absolute -right-20 bottom-12 z-[100] tiny:hidden">
-          <Image src="/images/waves.svg" width={181} height={116} alt="waves" />
-        </div>
+        <Waves right={-80} bottom={48} />
 
         <div className="overflow-hidden mid2:mt-16">
           <Image
