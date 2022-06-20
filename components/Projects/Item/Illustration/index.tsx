a91dec782d7b1b2bc5028e810cb914ef8e19312e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-20 12:22:25 +0100

    feat: portfolio items illustration transitions

--- components/Projects/Item/Illustration/index.tsx
+++ components/Projects/Item/Illustration/index.tsx
@@ -14,9 +14,9 @@ const Illustration: React.FC<IllustrationProps> = ({
   activeTag,
 }) => {
   return (
-    <div className={`relative`}>
+    <div className={`relative cursor-pointer group`}>
       <div
-        className="relative mid:mt-14 xs:mt-10 projects__card"
+        className="relative mid:mt-14 xs:mt-10 projects__card smooth hover:-translate-y-2 hover:translate-x-2"
         style={{
           boxShadow:
             activeTag !== 'Backend' ? '0px 4px 40px rgba(0, 0, 0, 0.07)' : '',
@@ -28,6 +28,21 @@ const Illustration: React.FC<IllustrationProps> = ({
           height={height}
           alt="team" //TODO
         />
+      </div>
+
+      <div className="overflow-hidden opacity-0 group-hover:opacity-100 flex items-center absolute -bottom-8 right-0 transition-opacity">
+        <h5 className="font-bold -translate-x-8 group-hover:translate-x-0 smooth text-[14px] mr-4 text-[#BAB7BC]">
+          See this project
+        </h5>
+
+        <div className="mt-[6px] -translate-x-8 group-hover:translate-x-0 smooth">
+          <Image
+            src="https://uploads-ssl.webflow.com/5e1e0172db1e6f151e507c06/5e1e0172db1e6feadc507c16_ic-arrow-forward-18px.svg"
+            width={35}
+            height={15}
+            alt="random" //TODO
+          />
+        </div>
       </div>
 
       {activeTag !== 'Backend' && (
