2d8b09eef60f230ca58bb654b0e295b5858a2209
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-22 18:39:08 +0100

    test: testing if hooks affect performance

--- components/Projects/Item/index.tsx
+++ components/Projects/Item/index.tsx
@@ -1,6 +1,7 @@
 import Illustration, { IllustrationProps } from './Illustration';
 import Content, { ContentProps } from './Content';
 import { isEven } from '../../../utils';
+import { useWindowWidth } from '../../../hooks';
 
 interface ItemInterface {
   content: ContentProps;
@@ -15,6 +16,8 @@ const PortolioItem: React.FC<ItemInterface> = ({
   order,
   activeTag,
 }) => {
+  const width = useWindowWidth();
+
   const imgConfig = { ...image, activeTag };
 
   return (
@@ -22,7 +25,7 @@ const PortolioItem: React.FC<ItemInterface> = ({
       data-aos="fade-up"
       className={`project__item flex justify-between items-center mt-28 mid:mt-12 xs:mt-0 gap-x-20 mid:flex-col mid:items-start mid:mx-auto`}
     >
-      {isEven(order) ? (
+      {isEven(order) || width! < 1000 ? (
         <>
           <Content {...content} />
           <Illustration {...imgConfig} />
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,1 @@
-// export * from './useWindowWidth';
-export {};
+export * from './useWindowWidth';
