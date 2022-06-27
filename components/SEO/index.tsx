167cbf5daaf1dbf19f44143458814b62ab8740ac
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-27 18:18:46 +0100

    feat: sitemaps

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -17,10 +17,7 @@ const SEO: React.FC<SEOProps> = ({ title, description, keywords }) => {
       {keywords && <meta name="keywords" content={keywords} />}
 
       <meta name="author" content="Achraf Elmouhib" />
-      <meta
-        name="google-site-verification"
-        content="Xumfw9BJVVesBYRZOYzhlHsveF54cvXUjxFJi4LWJXE"
-      />
+      <meta name="google-site-verification" content="OdNjxBOWAKLRxcPw1mjjzOpIRjFoccL5r-0ann3QDOU" />
     </Head>
   );
 };
